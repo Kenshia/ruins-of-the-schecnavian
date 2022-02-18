645bb2969e7b6ff775c6c5159d29bc82a69db9e3
WilsonNathaniel <wilsonnathaniel955@gmail.com>
2022-02-18 21:11:30 +0700

    finish screen done (i hope)

--- Jam Buddies Minggu 3 Project/Assets/Scripts/FinishAndTime.cs
+++ Jam Buddies Minggu 3 Project/Assets/Scripts/FinishAndTime.cs
@@ -8,6 +8,7 @@ public class FinishAndTime : MonoBehaviour
     public int level;
     public GameObject finishScreen;
     public TextMeshProUGUI timeText;
+    public TextMeshProUGUI FinishTimeText;
     private float time;
 
     private void Start()
@@ -27,6 +28,7 @@ public class FinishAndTime : MonoBehaviour
     {
         Time.timeScale = 0f;
         finishScreen.SetActive(true);
+        FinishTimeText.text = "Time Spent : " + ((int)time / 60).ToString("00") + ":" + (time % 60).ToString("00");
         int idx = level - 1;
         float best = PlayerPrefs.GetFloat("BestTime" + idx.ToString(), 0f);
         if(best == 0 || time < best)
