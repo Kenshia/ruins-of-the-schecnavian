eae284aeb81d87c9bc6b8133cdebfc8fc58ba69b
Kenshia <73539778+Kenshia@users.noreply.github.com>
2022-02-19 14:18:59 +0700

    water + lv 5 (not done)

---
+++ Jam Buddies Minggu 3 Project/Assets/Scripts/Water.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    public Sprite[] sprites;
+    private SpriteRenderer sr;
+    private float timePerFrame;
+    private int i;
+    private void Start()
+    {
+        timePerFrame = 1f;
+        sr = GetComponent<SpriteRenderer>();
+        Events.realWorld.AddListener(OnReal);
+        Events.unrealWorld.AddListener(OnUnreal);
+    }
+    private void FixedUpdate()
+    {
+        timePerFrame -= Time.deltaTime;
+        if(timePerFrame <= 0)
+        {
+            timePerFrame = 1f;
+            sr.sprite = sprites[i % 3];
+            i = (i + 1) % 3;
+        }
+    }
+
+    private void OnReal()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void OnUnreal()
+    {
+        gameObject.SetActive(false);
+    }
+}
