ccd055cdf02a87c3c12e822f7372bc7fa23fc331
Kenshia <73539778+Kenshia@users.noreply.github.com>
2022-02-26 21:32:58 +0700

    fix button sound

--- Jam Buddies Minggu 3 Project/Assets/Scripts/Audio/ButtonSound.cs
+++ Jam Buddies Minggu 3 Project/Assets/Scripts/Audio/ButtonSound.cs
@@ -6,6 +6,11 @@ public class ButtonSound : MonoBehaviour
 {
     public void PlayButtonSound()
     {
+        AudioManager.instance.PlayS("button");
+    }
+
+    public void PlayWritingSound()
+    {
         if (Random.Range(0, 100) < 100)
             AudioManager.instance.PlayS("writing1");
         else
